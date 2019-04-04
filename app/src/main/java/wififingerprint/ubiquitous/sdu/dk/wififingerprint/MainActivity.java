49be21ea311d733ddaf4db58dae88075f5d1654a
Mikenno <danielandersen159@gmail.com>
2019-04-04 14:16:50 +0200

    Baseline

---
+++ app/src/main/java/wififingerprint/ubiquitous/sdu/dk/wififingerprint/MainActivity.java
@@ -0,0 +1,13 @@
+package wififingerprint.ubiquitous.sdu.dk.wififingerprint;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
